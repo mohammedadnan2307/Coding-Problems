9ade1d4cd3c625e77e867c3b97e7291671ee4b41
mohammedadnan2307 <55001351+mohammedadnan2307@users.noreply.github.com>
2021-08-17 16:08:11 +0530

    Update solution.cpp

--- kick-start/Countdown/solution.cpp
+++ kick-start/Countdown/solution.cpp
@@ -1,1 +1,36 @@
-
+#include<iostream>
+using namespace std;
+unsigned int countDown(unsigned int n, unsigned int k)
+{
+    unsigned int element, no_countdown = 0, temp = k;
+    for(int i=0;i<n;++i)
+    {
+        cin>>element;
+        if(element == temp)
+        {
+            if(element == 1)
+            {
+                temp = k;
+                ++no_countdown;
+            }
+            else --temp;
+        }
+        else if(element == k)
+        {
+            temp = k-1;
+        }
+        else temp = k;
+    }
+    return no_countdown;
+}
+int main()
+{
+    unsigned int testCase, n, k;
+    cin>>testCase;
+    for(unsigned int i = 0; i < testCase; ++i)
+    {
+        cin>>n>>k;
+        cout<<"Case #"<<i+1<<": "<<countDown(n,k)<<endl;
+    }
+    return 0;
+}
